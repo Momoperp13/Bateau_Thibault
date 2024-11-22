7d284105d8da749e0de608724f468d8456370cdd
Yousfi Mohamed <mohamedlazhar27@hotmail.fr>
2024-11-22 22:07:48 +0100

    j'ai rajouté les pages + fichiers json (à compléter par les liens vers les images qui seront stockés dans le dossier assets/images

--- Bateau_Tibault/src/app/app.routes.ts
+++ Bateau_Tibault/src/app/app.routes.ts
@@ -10,4 +10,24 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'recettes',
+    loadComponent: () => import('./recettes/recettes.page').then( m => m.RecettesPage)
+  },
+  {
+    path: 'restaurants',
+    loadComponent: () => import('./restaurants/restaurants.page').then( m => m.RestaurantsPage)
+  },
+  {
+    path: 'bateaux',
+    loadComponent: () => import('./bateaux/bateaux.page').then( m => m.BateauxPage)
+  },
+  {
+    path: 'produits',
+    loadComponent: () => import('./produits/produits.page').then( m => m.ProduitsPage)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./contact/contact.page').then( m => m.ContactPage)
+  },
 ];
