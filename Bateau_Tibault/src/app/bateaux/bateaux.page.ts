2c0f1358a66029a416886cb4deb9e7c20f5c87a2
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-11-25 16:37:32 +0100

    page panier

--- Bateau_Tibault/src/app/bateaux/bateaux.page.ts
+++ Bateau_Tibault/src/app/bateaux/bateaux.page.ts
@@ -1,20 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonIcon,
+  IonButtons,
+  IonCard,
+  IonItem,
+  IonLabel,
+  IonThumbnail,
+} from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-bateaux',
   templateUrl: './bateaux.page.html',
   styleUrls: ['./bateaux.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButton,
+    IonIcon,
+    IonButtons,
+    IonCard,
+    IonItem,
+    IonLabel,
+    IonThumbnail,
+  ],
 })
 export class BateauxPage implements OnInit {
+  constructor() {}
+  currentDate: Date = new Date();
 
-  constructor() { }
+  products = [
+    {
+      name: 'Produit 1',
+      image: 'assets/images/daurade.jpg',
+      quantity: 2,
+      price: 20.0,
+    },
+    {
+      name: 'Produit 2',
+      image: 'assets/images/daurade.jpg',
+      quantity: 1,
+      price: 15.5,
+    },
+    {
+      name: 'Produit 3',
+      image: 'assets/images/daurade.jpg',
+      quantity: 3,
+      price: 10.0,
+    },
+  ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
